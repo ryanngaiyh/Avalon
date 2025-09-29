4dc66d48f8b78c9ddc66816ab1605cf6a534fd2f
ryanngaiyh <ryanngaiyh@gmail.com>
2025-09-29 17:09:13 +0800

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,7 @@
+// supabase.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+export const supabase = createClient(
+  'https://arpeyanztddyhczqhdyw.supabase.co', 
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFycGV5YW56dGRkeWhjenFoZHl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxMzE0NDEsImV4cCI6MjA3NDcwNzQ0MX0.sjaMbgqkfxTf4XmYd-bubWmjNJtiFunFv_WLEElBIpo'
+)
